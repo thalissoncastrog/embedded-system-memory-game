13e38528eac361600f0147f0dad20c23417ba47f
Thalisson Castro <adaothalisson@hotmail.com>
2025-02-25 20:56:09 -0300

    refactor: reorganize the code to make it more readable

--- memory-game.c
+++ memory-game.c
@@ -7,6 +7,7 @@
 #include "lib/interruptions_counter.pio.h"
 #include "lib/ssd1306_i2c.h"
 
+// Definições de constantes
 #define NUM_PIXELS 25
 #define LED_MATRIX_PIN 7
 
@@ -24,22 +25,38 @@
 #define I2C_SDL 15
 #define ADDRESS 0x3C
 
-
 #define IS_RGBW false
 
 #define DEBOUNCE_US 200000  // 200ms debounce
 
-#define MAX_NUMBERS 999
+#define MAX_NUMBERS 20
 
+// Variáveis globais
 static int confirmed_numbers[MAX_NUMBERS];
 static int current_index = 0;
 
-static volatile uint COUNTER = 0;
+// Variáveis globais para controlar o estado dos LEDs e o temporizador
+volatile bool led_on = false;
+volatile alarm_id_t led_alarm_id = -1;
 
-uint8_t led_r = 0; // Red intensity
-uint8_t led_g = 0; // Green intensity
-uint8_t led_b = 20; // Blue intensity
+static volatile uint COUNTER = 0; //Contador para mostrar os valores na matriz de LEDs
 
+uint8_t led_r = 0; // Intensidade de vermelho
+uint8_t led_g = 0; // Intensidade de verde
+uint8_t led_b = 20; // Intensidade de azul
+
+// Variáveis para controlar a sequência de números
+static int sequence[MAX_NUMBERS];
+static int sequence_length = 1;
+static int current_step = 0;
+static int score = 0;
+
+int upper_bound = 9;
+int lower_bound = 0;
+
+ssd1306_t display;
+
+// Padrões de LEDs para exibir números
 bool led_number_0[NUM_PIXELS] = {
     0, 1, 1, 1, 0,
     1, 1, 0, 0, 1,
@@ -128,104 +145,49 @@ bool turn_off_all_leds[NUM_PIXELS] = {
     0, 0, 0, 0, 0,
 };
 
-bool* draw_number(int number);
-void set_one_led(uint8_t r, uint8_t g, uint8_t b);
-static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b);
-static inline void put_pixel(uint32_t pixel_grb);
-void gpio_irq_handler(uint gpio, uint32_t events);
+// Protótipos de funções
+void setup();
+void init_hardware();
 
 void init_pwm(uint pin);
 void set_pwm_duty_cycle(uint pin, uint16_t duty_cycle);
+
+void set_one_led(uint8_t r, uint8_t g, uint8_t b);
+static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b);
+static inline void put_pixel(uint32_t pixel_grb);
+bool* draw_number(int number);
+int64_t clear_display_callback(alarm_id_t id, void *user_data);
+
+void interruptions_callback_config();
+void gpio_irq_handler(uint gpio, uint32_t events);
 
 void correct_answer();
 void wrong_answer();
 
 void display_score();
-void wait_for_restart();
 
-int64_t clear_display_callback(alarm_id_t id, void *user_data);
+int64_t reset_game();
 
+void show_sequence();
+void generate_sequence();
 
-
-static int sequence[MAX_NUMBERS];
-static int sequence_length = 1;
-static int current_step = 0;
-static int score = 0;
-
-int upper_bound = 9;
-int lower_bound = 0;
-
-ssd1306_t display;
-
-void generate_sequence() {
-    srand(time(NULL));
-    for (int i = 0; i < sequence_length; i++) {
-        sequence[i] = rand() % (upper_bound - lower_bound + 1) + lower_bound;;
-    }
-}
-
-void show_sequence() {
-    printf("Sequence: ");
-    for (int i = 0; i < sequence_length; i++) {
-        printf("%d ", sequence[i]);
-    }
-    printf("\n");
-
-    bool color = true;
-    color = !color;
-
-    ssd1306_fill(&display, !color); // Limpa o display
-    ssd1306_rect(&display, 3, 3, 122, 58, color, !color); // Desenha um retângulo
-    char buffer[32];
-    snprintf(buffer, sizeof(buffer), "Sequence: ");
-    ssd1306_draw_string(&display, buffer, 8, 5);
-
-    for (int i = 0; i < sequence_length; i++) {
-        snprintf(buffer, sizeof(buffer), "%d ", sequence[i]);
-
-        // ssd1306_draw_string(&display, buffer, (i + 1) * 10, 30); // Desenha uma string
-
-        // if(sequence_length >= 11){
-        //     ssd1306_draw_string(&display, buffer, (i + 1) * 10, 40); // Desenha uma string
-        // }
-
-        // Calcula a posição x e y para exibir o número
-
-        int x = (i % 11) * 10 + 8;
-        int y = (i / 11) * 10 + 20;
-
-        ssd1306_draw_string(&display, buffer, x, y); // Desenha uma string
-
-
-    }
-
-    // // Atualiza o conteúdo do display com animações
-    // ssd1306_draw_string(&display, "Botao Errado", 8, 10); // Desenha uma string
-    // ssd1306_draw_string(&display, "Fim de jogo", 20, 30); // Desenha uma string
-    ssd1306_send_data(&display); // Atualiza o display
-
-    add_alarm_in_ms(2000, clear_display_callback, NULL, false);
-}
-
-// Função de callback do temporizador para apagar o display
-int64_t clear_display_callback(alarm_id_t id, void *user_data) {
-    ssd1306_fill(&display, 0); // Limpa o display
-    ssd1306_send_data(&display); // Atualiza o display
-    return 0;
-}
-
-int64_t reset_game() {
-    sequence_length = 1;
-    current_step = 0;
-    score = 0;
-    generate_sequence();
-    show_sequence();
-
-    return 0;
-}
 
 int main()
 {
+    setup();
+
+    init_hardware();
+
+    interruptions_callback_config();
+
+    generate_sequence();
+    show_sequence();
+
+    while (true) {}
+}
+
+// Função de configuração dos pinos
+void setup(){
     stdio_init_all();
 
     gpio_init(BUTTON_A_PIN);
@@ -248,6 +210,10 @@ int main()
     gpio_init(BUTTON_JOYSTICK_PIN);
     gpio_set_dir(BUTTON_JOYSTICK_PIN, GPIO_IN); // Configura o pino como entrada
     gpio_pull_up(BUTTON_JOYSTICK_PIN);          // Habilita o pull-up interno
+}
+
+// Função para inicializar o hardware PWM, I2C, display OLED e PIO
+void init_hardware(){
 
     // Inicializa PWM para os LEDs RGB
     init_pwm(RED_LED_PIN);
@@ -273,45 +239,32 @@ int main()
     int sm = 0;
     uint offset = pio_add_program(pio, &interruptions_counter_program);
 
+    interruptions_counter_program_init(pio, sm, offset, LED_MATRIX_PIN, 800000, IS_RGBW);
+    set_one_led(led_r, led_g, led_b);
+}
+
+// Função para configurar as interrupções com callback dos botões A, B e Joystick
+void interruptions_callback_config(){
     // Configuração da interrupção com callback
     gpio_set_irq_enabled_with_callback(BUTTON_A_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
     gpio_set_irq_enabled(BUTTON_B_PIN, GPIO_IRQ_EDGE_FALL, true);
     gpio_set_irq_enabled(BUTTON_JOYSTICK_PIN, GPIO_IRQ_EDGE_FALL, true);
+}
 
-    interruptions_counter_program_init(pio, sm, offset, LED_MATRIX_PIN, 800000, IS_RGBW);
-
-    set_one_led(led_r, led_g, led_b);
-
-    generate_sequence();
-    show_sequence();
-
-    while (true) {
-        sleep_ms(1000);
-
-        // if(sizeof(confirmed_numbers) > 0){
-        //     for(int i = 0; i < current_index; i++){
-        //         printf("Number %d: %d\n", i, confirmed_numbers[i]);
-        //     }
-        // }
-
-        // for(int i = 0; i < 5; i++){
-        //     int value = rand() % (upper_bound - lower_bound + 1) + lower_bound;
-            
-        //     if(i == 4){
-        //         printf("%d\n", value);
-        //     }else{
-        //         printf("%d, ", value);
-        //     }
-        // }
-
-        // printf("--------------------------------------------------------------\n");
+// Função para gerar uma sequência de números aleatóriosS
+void generate_sequence(){
+    srand(time(NULL));
+    for (int i = 0; i < sequence_length; i++) {
+        sequence[i] = rand() % (upper_bound - lower_bound + 1) + lower_bound;;
     }
 }
 
+// Função para configurar o duty cycle do PWM
 void set_pwm_duty_cycle(uint pin, uint16_t duty_cycle){
     pwm_set_gpio_level(pin, duty_cycle);
 }
 
+// Função para inicializar o PWM
 void init_pwm(uint pin){
     gpio_set_function(pin, GPIO_FUNC_PWM);
     uint slice_num = pwm_gpio_to_slice_num(pin);
@@ -320,6 +273,52 @@ void init_pwm(uint pin){
     pwm_set_enabled(slice_num, true);
 }
 
+// Função para reiniciar o jogo
+int64_t reset_game() {
+    sequence_length = 1;
+    current_step = 0;
+    score = 0;
+    generate_sequence();
+    show_sequence();
+
+    return 0;
+}
+
+// Função para exibir a sequência de números no display OLED
+void show_sequence() {
+    printf("Sequence: ");
+    for (int i = 0; i < sequence_length; i++) {
+        printf("%d ", sequence[i]);
+    }
+    printf("\n");
+
+    bool color = true;
+    color = !color;
+
+    ssd1306_fill(&display, !color); // Limpa o display
+    ssd1306_rect(&display, 3, 3, 122, 58, color, !color); // Desenha um retângulo
+    char buffer[32];
+    snprintf(buffer, sizeof(buffer), "Sequence: ");
+    ssd1306_draw_string(&display, buffer, 8, 5);
+
+    for (int i = 0; i < sequence_length; i++) {
+        snprintf(buffer, sizeof(buffer), "%d ", sequence[i]);
+
+        // Calcula a posição x e y para exibir o número
+        int x = (i % 11) * 10 + 8;
+        int y = (i / 11) * 10 + 20;
+
+        ssd1306_draw_string(&display, buffer, x, y); // Desenha uma string
+
+
+    }
+
+    ssd1306_send_data(&display); // Atualiza o display
+
+    add_alarm_in_ms(2000, clear_display_callback, NULL, false);
+}
+
+// Função para desenhar um número na matriz de LEDs
 bool* draw_number(int number)
 {
     switch (number)
@@ -360,6 +359,7 @@ bool* draw_number(int number)
     }
 }
 
+// Função para enviar um pixel para a máquina de estado do PIO
 static inline void put_pixel(uint32_t pixel_grb)
 {
     // Envia o valor do pixel para a máquina de estado do PIO, deslocando 8 bits para a esquerda.
@@ -373,6 +373,7 @@ static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b)
     return ((uint32_t)(r) << 8) | ((uint32_t)(g) << 16) | (uint32_t)(b);
 }
 
+// Função para definir a cor de um LED na matriz de LEDs
 void set_one_led(uint8_t r, uint8_t g, uint8_t b)
 {
     // Define a cor com base nos parâmetros fornecidos
@@ -394,9 +395,12 @@ void set_one_led(uint8_t r, uint8_t g, uint8_t b)
     }
 }
 
-// Variáveis globais para controlar o estado dos LEDs e o temporizador
-volatile bool led_on = false;
-volatile alarm_id_t led_alarm_id = -1;
+// Função de callback do temporizador para apagar o display
+int64_t clear_display_callback(alarm_id_t id, void *user_data){
+    ssd1306_fill(&display, 0); // Limpa o display
+    ssd1306_send_data(&display); // Atualiza o display
+    return 0;
+}
 
 // Função de callback do temporizador para desligar os LEDs
 int64_t led_off_callback(alarm_id_t id, void *user_data) {
@@ -422,11 +426,6 @@ void gpio_irq_handler(uint gpio, uint32_t events)
     } else if (gpio == BUTTON_A_PIN) {
         COUNTER = (COUNTER - 1 + 10) % 10;
     }else if (gpio == BUTTON_JOYSTICK_PIN) {
-        // if(current_index < MAX_NUMBERS){
-        //     confirmed_numbers[current_index] = COUNTER;
-        //     current_index++;
-        //     printf("Number %d confirmed\n", confirmed_numbers[current_index-1]);
-        // }
 
         if(COUNTER == sequence[current_step]){
             current_step++;
@@ -442,24 +441,19 @@ void gpio_irq_handler(uint gpio, uint32_t events)
             correct_answer();
 
         }else{
-
-
             printf("Game Over!\nScore: %d\n", score);
 
             wrong_answer();
 
             display_score();
-
-            // wait_for_restart();
-
         }
-
     }
-
+    
     set_one_led(led_r, led_g, led_b);
     last_interrupt_time = current_time;
 }
 
+// Função para exibir a resposta correta
 void correct_answer(){
     set_pwm_duty_cycle(RED_LED_PIN, 0);
     set_pwm_duty_cycle(GREEN_LED_PIN, 20);
@@ -471,6 +465,7 @@ void correct_answer(){
     led_alarm_id = add_alarm_in_ms(500, led_off_callback, NULL, false);
 }
 
+// Função para exibir a resposta errada
 void wrong_answer(){
     set_pwm_duty_cycle(RED_LED_PIN, 20);
     set_pwm_duty_cycle(GREEN_LED_PIN, 0);
@@ -482,6 +477,7 @@ void wrong_answer(){
     led_alarm_id = add_alarm_in_ms(500, led_off_callback, NULL, false);
 }
 
+// Função para exibir a pontuação
 void display_score() {
     char buffer[32];
     ssd1306_fill(&display, 0); // Limpa o display
@@ -494,14 +490,4 @@ void display_score() {
     }
 
     led_alarm_id = add_alarm_in_ms(2000, reset_game, NULL, false);
-}
-
-void wait_for_restart() {
-    while (true) {
-        if (gpio_get(BUTTON_JOYSTICK_PIN) == 0) { // Verifica se o botão de joystick foi pressionado
-            reset_game();
-            break;
-        }
-        // sleep_ms(100); // Aguarda 100ms antes de verificar novamente
-    }
 }
